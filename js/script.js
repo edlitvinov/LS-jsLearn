964209d10fe1df436b3eecb009970b78b63eb052
edlitvinov <edoss@list.ru>
2022-05-24 00:08:13 +0400

    function type

--- js/script.js
+++ js/script.js
@@ -1,18 +1,42 @@
 'use strict';
 
-// замыкание
+// стрелочные функции
 
-let a = 12;
+let gaf = (a, b) => a * b;
+console.log(gaf(2, 6));
 
-function f1(b, c) {
-    function f2(d, e) {
-        return a + b + c + d + e;
-    }
-    return f2(4, 7);
+let ar = [1, 2, 4, 6, 7];
+let newAr = ar.map(n => n * n);
+console.log(ar);
+console.log(newAr);
+
+// function Expression
+
+let shg = function(a, b) {  // переменная sum создаётся явно. функция внутри какого то выражения
+    return a + b;
 }
 
-let res = f1(8, 2);
-console.log(res);
+// function Declaration 
+
+function sum(a, b) {   // интерпретатор сам создаст переменную sum
+    return a + b;
+}
+let d = sum(15, 3);
+console.log(d);
+
+// замыкание
+
+// let a = 12;
+
+// function f1(b, c) {
+//     function f2(d, e) {
+//         return a + b + c + d + e;
+//     }
+//     return f2(4, 7);
+// }
+
+// let res = f1(8, 2);
+// console.log(res);
 
 // функции
 
