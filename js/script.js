b3607ed310d8409f6defbfccd2fceccc0c97e22d
edlitvinov <edoss@list.ru>
2022-05-23 22:45:37 +0400

    замыкания

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+let a = 15;
+
+function f1(b, c) {
+    function f2(d, e) {
+        return a + b + c + d + e;
+    }
+    return f2(4, 7);
+}
+
+let res = f1(8, 2);
+console.log(res);
