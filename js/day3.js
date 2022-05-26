a2e5388ec96551e9869339a9d8cdc126a1c20ac7
edlitvinov <edoss@list.ru>
2022-05-26 16:50:45 +0400

    navigation DOM

--- js/day3.js
+++ js/day3.js
@@ -1,23 +1,43 @@
 'use strict';
 
-const myButton = document.querySelector('#myButton');
-const nameInput = document.querySelector('#nameInput');
-const list = document.querySelector('#list');
+// const body = document.body;
 
-myButton.addEventListener('click', function() {
-    const newItem = document.createElement('li');
-    newItem.classList.add('item');
-    newItem.textContent = nameInput.value;
+// console.log(body.firstElementChild.nextElementSibling.previousElementSibling);
 
-    const delButton = document.createElement('button');
-    delButton.textContent = 'delete';
-    delButton.addEventListener('click', function() {
-        list.removeChild(newItem);
-    });
 
-    newItem.appendChild(delButton);
-    list.appendChild(newItem);
-    
-    nameInput.value = '';
+// for (const node of body.children) {
+//     console.log(node);
+// }
 
-});
+// console.log(body.childNodes);
+
+// for (const node of body.childNodes) {
+//     if(node.nodeType === 3) {
+//         console.log('text type', node);
+//     } else  if(node.nodeType === 1) {
+//         console.log('element type', node);
+//     };
+// }
+
+
+// const myButton = document.querySelector('#myButton');
+// const nameInput = document.querySelector('#nameInput');
+// const list = document.querySelector('#list');
+
+// myButton.addEventListener('click', function() {
+//     const newItem = document.createElement('li');
+//     newItem.classList.add('item');
+//     newItem.textContent = nameInput.value;
+
+//     const delButton = document.createElement('button');
+//     delButton.textContent = 'delete';
+//     delButton.addEventListener('click', function() {
+//         list.removeChild(newItem);
+//     });
+
+//     newItem.appendChild(delButton);
+//     list.appendChild(newItem);
+
+//     nameInput.value = '';
+
+// });
